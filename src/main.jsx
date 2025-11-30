454b299b59afb24f186d0b4302d9eef2b574bd47
MakAILearn <maklearner3@gmail.com>
2025-11-30 11:37:06 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,8 @@
 
+// src/main.jsx
+import React from "react";
+import { createRoot } from "react-dom/client";
+import Landing from "./Landing";
+import "./index.css";
+
+createRoot(document.getElementById("root")).render(<Landing />);
